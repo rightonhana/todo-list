dde6b3703b29ec47bac3106c83cc33397e7c34a0
Florencia Gonzalez <rightonhana@gmail.com>
2021-05-26 13:20:08 -0700

    :sparkles: add AddItem component

---
+++ src/components/AddItem/AddItem.tsx
@@ -0,0 +1,32 @@
+import Button from "@material-ui/core/Button";
+import TextField from "@material-ui/core/TextField";
+import { FC, FormEvent, useState } from "react";
+import { useDispatch } from "react-redux";
+import { addTodo } from "../../features/todoSlice";
+import styles from "./AddItem.module.css";
+
+export const AddItem: FC<{}> = ({ ...props }) => {
+	const [input, setInput] = useState("");
+	const dispatch = useDispatch();
+
+	const onSubmit = (event: FormEvent<HTMLFormElement>) => {
+		event.preventDefault();
+		dispatch(
+			addTodo({
+				value: input,
+				done: false,
+				id: Date.now()
+			})
+		);
+		setInput("");
+	};
+
+	return (
+		<form className={styles.Form} onSubmit={onSubmit} {...props}> 
+			<TextField className={styles.Input} value={input} onChange={(event) => setInput(event.target.value)} label="Task" variant="outlined" />
+			<Button className={styles.Button} variant="contained" type="submit" color="primary">Add</Button>
+		</form> 
+	);
+};
+
+export default AddItem;
---
+++ src/components/AddItem/index.ts
@@ -0,0 +1,1 @@
+export { default, AddItem } from "./AddItem";
